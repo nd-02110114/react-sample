74c24cbe56197586be07cba3306c57931daf7e82
nd-02110114 <nd.12021218@gmail.com>
2017-06-29 09:36:01 +0900

    setting eslint

---
+++ src/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  parser: "babel-eslint",
+  ecmaFeatures: {
+    jsx: true
+  },
+  plugins: ["react", "jsx-a11y", "import"],
+  rules: {
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "react/prefer-stateless-function": [0, { ignorePureComponents: false }],
+    "class-methods-use-this": [0],
+    // disallow declaration of variables that are not used in the code
+    "no-unused-vars": [0, { vars: "all", args: "after-used" }],
+    // specify the maximum length of a line in your program
+    // http://eslint.org/docs/rules/max-len
+    "max-len": [
+      2,
+      110,
+      2,
+      {
+        ignoreUrls: true,
+        ignoreComments: false,
+        ignoreStrings: true
+      }
+    ],
+    "react/prop-types": [0, { ignore: "navigator" }]
+  },
+  globals: {
+    document: true,
+    window: true,
+    navigator: true,
+    fetch: true
+  }
+};
